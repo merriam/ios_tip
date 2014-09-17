27bfe34d1ee4f0cbeca131cb063d4cf349d7e56d
Charles Merriam <charles.merriam@gmail.com>
2014-09-16 20:35:22 -0700

    Created.   Emoji aren't rendering in selected/reversed text.  Icon and splash screen generate ignorable warnings.

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var percentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.1, 0.15, 0.20, 0.25]
+        var tipPercentage = tipPercentages[percentControl.selectedSegmentIndex]
+        var billAmount = NSString(string:billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true) // turn off keyboard
+    }
 }
 
